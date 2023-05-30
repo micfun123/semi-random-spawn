1394bef1539a3991d8c536dbdb234270b02af56d
Michael Parker <54761507+micfun123@users.noreply.github.com>
2023-05-30 20:57:51 +0100

    Add files via upload

---
+++ src/main/java/tech/michaelparker/semirandomspawn/Semi_random_spawn.java
@@ -0,0 +1,17 @@
+package tech.michaelparker.semirandomspawn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Semi_random_spawn extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
