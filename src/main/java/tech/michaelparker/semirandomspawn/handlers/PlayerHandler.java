97541440d7ff9579345989f98cb4d791c839369b
Michael Parker <michaelrbparker@gmail.com>
2023-05-31 18:16:59 +0100

    Zone selection

--- src/main/java/tech/michaelparker/semirandomspawn/handlers/PlayerHandler.java
+++ src/main/java/tech/michaelparker/semirandomspawn/handlers/PlayerHandler.java
@@ -1,46 +1,76 @@
 package tech.michaelparker.semirandomspawn.handlers;
 
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.entity.Player;
 import tech.michaelparker.semirandomspawn.Semi_random_spawn;
 
-public class PlayerHandler implements org.bukkit.event.Listener{
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class PlayerHandler implements Listener {
+    private final Semi_random_spawn plugin;
+
     public PlayerHandler(Semi_random_spawn plugin) {
-        Bukkit.getPluginManager().registerEvents((Listener)this, (org.bukkit.plugin.Plugin)plugin);
+        this.plugin = plugin;
+        Bukkit.getPluginManager().registerEvents(this, plugin);
     }
 
     @EventHandler
-    public void onPlayerJoin(org.bukkit.event.player.PlayerJoinEvent event) {
-        org.bukkit.entity.Player player = event.getPlayer();
-        // new players only
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        // Only execute for new players
         if (player.hasPlayedBefore()) {
             return;
-        }
-        else {
-            //get config welcome message
-            FileConfiguration config = Semi_random_spawn.getPlugin(Semi_random_spawn.class).getConfig();
+        } else {
+            // Get config welcome message
+            FileConfiguration config = plugin.getConfig();
             String welcomeMessage = config.getString("WelcomeMessage");
-            // send if not empty
-            if (welcomeMessage.isEmpty()) {
-                return;
-            }
-            else {
+            // Send if not empty
+            if (!welcomeMessage.isEmpty()) {
                 welcomeMessage = welcomeMessage.replace("%player%", player.getName());
                 player.sendMessage(welcomeMessage);
             }
 
+            ConfigurationSection zonesSection = config.getConfigurationSection("Zones");
+            if (zonesSection != null) {
+                List<String> zoneKeys = new ArrayList<>(zonesSection.getKeys(false));
+                if (!zoneKeys.isEmpty()) {
+                    // Select a random zone
+                    Random random = new Random();
+                    String randomZoneKey = zoneKeys.get(random.nextInt(zoneKeys.size()));
+                    ConfigurationSection randomZone = zonesSection.getConfigurationSection(randomZoneKey);
 
-            //get random location
-            double x = Math.random() * 1000;
-            double z = Math.random() * 1000;
-            double y = player.getWorld().getHighestBlockYAt((int) x, (int) z);
-            //teleport player
-            player.teleport(new org.bukkit.Location(player.getWorld(), x, y, z));
-            player.setBedSpawnLocation(new org.bukkit.Location(player.getWorld(), x, y, z));
+                    // Fetch zone coordinates
+                    int x1 = randomZone.getInt("x1");
+                    int z1 = randomZone.getInt("z1");
+                    int x2 = randomZone.getInt("x2");
+                    int z2 = randomZone.getInt("z2");
+
+                    // Calculate random coordinates within the zone
+                    int randomX = getRandomCoordinate(x1, x2);
+                    int randomZ = getRandomCoordinate(z1, z2);
+                    int y = player.getWorld().getHighestBlockYAt(randomX, randomZ);
+
+                    // Teleport player to the random location
+                    player.teleport(new org.bukkit.Location(player.getWorld(), randomX, y, randomZ));
+                    player.setBedSpawnLocation(new org.bukkit.Location(player.getWorld(), randomX, y, randomZ));
+
+                    // Send a welcome message with the zone information
+                    player.sendMessage("Welcome to Zone: " + randomZoneKey);
+                    player.sendMessage("Coordinates: X: " + randomX + ", Z: " + randomZ);
+                }
+            }
         }
-
     }
 
+    private int getRandomCoordinate(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min + 1) + min;
+    }
 }
